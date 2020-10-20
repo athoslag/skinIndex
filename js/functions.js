ef13fbf1c7317fc3afe80787a5aae8ee40a6a410
athoslag <athoslagemann@gmail.com>
2020-10-20 17:21:53 -0300

    Update inxed and css

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,50 @@
 function readFiles() { 
-	console.log("readFiles")
+	console.log("readFiles");
+
+	document.addEventListener("DOMContentLoaded", initializeEntries);
 }
 
-function downloadSkin(index, url) {
-	document.getElementById("download").src = 'skin/'+index+'.skin';
+function initializeEntries() { 
+	var i;
+	for (i = 1; i <= 3; i++) { 
+		addEntry(i);
+	}
+}
+
+function addEntry(index) { 
+	console.log("execution #" + index)
+
+	var entry = document.createElement("skin");   // Create a <button> element
+	entry.href = "index.css"
+	
+	let image = document.createElement("img");
+	image.id = "img_" + index;
+	image.alt = index;
+	image.src = 'src/' + index + '.jpg';
+	image.style = "width: 50%";
+
+	let button = document.createElement("button");
+	// button.id = "btn_" + index;
+	button.style.height = '20px';
+	button.style.width= '80px';
+	button.innerHtml = "Download";
+
+	// button.type = "button";
+	// button.addEventListener("onclick", downloadSkin(index));
+	// button.onclick = downloadSkin(index);
+	button.addEventListener ("click", function() {
+	  	downloadSkin(index);
+	});
+	
+	let p = document.createElement("p");
+	p.appendChild(button);
+
+	entry.appendChild(image);
+	entry.appendChild(p);
+	document.getElementById("skins").appendChild(entry);
+}
+
+function downloadSkin(index) {
+	console.log("download skin #" + index);
+	// document.getElementById("download").src = 'skin/'+index+'.skin';
 }
